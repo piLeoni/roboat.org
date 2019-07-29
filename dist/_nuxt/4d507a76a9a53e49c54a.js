28902bb7d97c59a772e9241cf5d67d4668497e0a
pileoni <pietro.leoni@gmail.com>
2019-07-29 11:55:42 -0400

    update and nyxt-link test

---
+++ dist/_nuxt/4d507a76a9a53e49c54a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{180:function(t,e,n){"use strict";n.r(e);var l=n(25),component=Object(l.a)({},function(){var t=this.$createElement;return(this._self._c||t)("h1",[this._v("this is a test")])},[],!1,null,null,null);e.default=component.exports}}]);
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         danceflood
       </h1>
       <h2 class="subtitle">
-        My first-class Nuxt.js project
+        Change test
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">
@@ -19,6 +19,11 @@
         >
           GitHub
         </a>
+      </div>
+      <div class="links">
+        <nuxt-link to="/testpage">
+          test
+        </nuxt-link>
       </div>
     </div>
   </div>
